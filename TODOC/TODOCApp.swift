dc20786f83c2ffc436b4628289cc53350c516186
Subham <patelsk7481@gmail.com>
2024-09-25 16:15:45 +0530

    Initial Commit

---
+++ TODOC/TODOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODOCApp.swift
+//  TODOC
+//
+//  Created by Subham Patel on 25/09/24.
+//
+
+import SwiftUI
+
+@main
+struct TODOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
